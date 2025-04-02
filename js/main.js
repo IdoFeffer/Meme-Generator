1982d864cc2d140bb645d6c55cc6802faf333095
Ido Feffer <idoppr1@gmail.com>
2025-04-02 11:10:00 +0300

    Add download

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 "use strict"
 
 function onInit() {
-    renderGallery()
-    renderMeme()
+  renderGallery()
+  renderMeme()
+}
+
+// download canvas
+function onDownloadCanvas(elLink) {
+  const canvas = document.getElementById("meme-canvas")
+  const dataUrl = canvas.toDataURL()
+  elLink.href = dataUrl
+  // Set a name for the downloaded file
+  elLink.download = "my-meme.jpg"
 }
